22108d625678aa90ed411b43a0f69e89b6ed2ae0
Vladimir Butov <devvlad@prodalet.ru>
2023-07-31 21:48:14 +0300

    Version 1.2

--- admin_panel.php
+++ admin_panel.php
@@ -1,14 +1,14 @@
 <?php
 
-class Prodalet
+class ProdaletAdmin
 {
 	var $script_mode_array = array(
 		'admin_not_display' => 'Отключать во время авторизации администратора сайта'
 		);
 
 	var $script_position_array = array(
-		'header' => 'Подключается в начале странице',
-		'footer' => 'Подключается в конце страницы'
+		'footer' => 'Подключается в конце страницы',
+        'header' => 'Подключается в начале странице',
 		);
 
 	function __construct()
@@ -18,7 +18,7 @@ class Prodalet
 	    add_action('admin_menu', array(&$this, 'admin_menu'));
 
 	    //Добавляем в описание плагина ссылку на справку.
-	    add_filter('plugin_row_meta', 'Prodalet::plugin_row_meta', 10, 2);
+	    add_filter('plugin_row_meta', 'ProdaletAdmin::plugin_row_meta', 10, 2);
 
 	    add_action('admin_init', array(&$this, 'plugin_settings'));
 	}
@@ -69,7 +69,11 @@ class Prodalet
 	 * Регистрируем настройки.
 	 * Настройки будут храниться в массиве, а не одна настройка = одна опция.
 	 */
-	function plugin_settings(){
+	function plugin_settings()
+    {
+
+        $domain = parse_url(site_url())['host'];
+
 		
 		// параметры: $option_group, $option_name, $sanitize_callback
 		register_setting( 'prodalet_group', 'prodalet', array(&$this, 'sanitize_callback') );
@@ -77,22 +81,57 @@ class Prodalet
 		// параметры: $id, $title, $callback, $page
 		add_settings_section( 'prodalet', '', array(&$this, 'display_setting_info') , 'prodalet_page' );
 
-		$field_params = array(
+		/*$field_params = array(
 			'type'      => 'textarea', 
 			'id'        => 'script',
 			'label_for' => 'script' 
 		);
 		add_settings_field( 'script', 'Код установки ProdaLet', array(&$this, 'display_settings'), 'prodalet_page', 'prodalet', $field_params );
+		*/
+
+        $field_params = array(
+            'type'      => 'text',
+            'id'        => 'script_api_key',
+            'label_for' => 'script_api_key'
+        );
+        add_settings_field( 'script_api_key', 'API KEY ProdaLet', array(&$this, 'display_settings'), 'prodalet_page', 'prodalet', $field_params );
+
+        $field_params = array(
+            'type'      => 'text',
+            'id'        => 'script_api_secret',
+            'label_for' => 'script_api_secret'
+        );
+        add_settings_field( 'script_api_secret', 'API SECRET ProdaLet', array(&$this, 'display_settings'), 'prodalet_page', 'prodalet', $field_params );
+
 	 
 	 	$field_params = array(
 			'type'      => 'select', 
 			'id'        => 'position',
 			'label_for' => 'position' ,
 			'desc'      => 'Выберите место расположения сервиса. Если установить наш код в начале странице - то виджеты будут показывать быстрее.', // описание
-			'vals' => $this->script_position_array
+			'vals' => $this->script_position_array,
+            'defautl' => 'footer',
 			);
 		add_settings_field( 'position', 'Подключение кода установки', array(&$this, 'display_settings'), 'prodalet_page', 'prodalet', $field_params );
-	 	 
+
+        $field_params = array(
+            'type'      => 'text',
+            'id'        => 'lazy_load_desctop',
+            'desc'      => 'Через сколько секунд после загрузки сайта будет подгружаться сервис Prodalet на компьютерах. 0 - сразу после загрузки. ', // описание
+            'label_for' => 'lazy_load_desctop',
+            'default' => 0,
+        );
+        add_settings_field( 'lazy_load_desctop', 'Отложенная загрузка для КОМПЬЮТЕРОВ, секунд ', array(&$this, 'display_settings'), 'prodalet_page', 'prodalet', $field_params );
+
+        $field_params = array(
+            'type'      => 'text',
+            'id'        => 'lazy_load_mobile',
+            'desc'      => 'Через сколько секунд после загрузки сайта на мобильных телефонах будет подгружаться сервис Prodalet . 0 - сразу после загрузки. ', // описание
+            'label_for' => 'lazy_load_mobile',
+            'default' => 0,
+        );
+        add_settings_field( 'lazy_load_mobile', 'Отложенная загрузка для МОБИЛЬНЫХ, секунд ', array(&$this, 'display_settings'), 'prodalet_page', 'prodalet', $field_params );
+
 		$field_params = array(
 			'type'      => 'checkbox-group', 
 			'id'        => 'mode',
@@ -112,8 +151,15 @@ class Prodalet
 	}
 
 	// Поясняющее сообщение для секции тестирования и отладки
-	function display_setting_info(){
-		echo '<p>Для работы виджетов вам необходимо получить код установки сервиса повышения конверсии <a href="https://prodlet.ru"  target="_blank">ProdaLet.ru</a> и вставить его ниже в соответствующее поле.</p>';
+	function display_setting_info()
+    {
+        $domain = parse_url(site_url())['host'];
+		print  "
+            <p>Для работы виджетов вам необходимо получить API KEY и API SECRET на странице проекта</p> 
+            <p>Кликните на название проекта (<b>\"{$domain}\"</b>) -> раздел 'API' или 'Код установки'</p>
+            <p><b><a href='https://prodalet.ru/projects/api/".$domain."/'  target='_blank'>Перейдите в свой аккаунт ProdaLet.ru</a></b>
+            </p>
+            ";
 	}
 
 	/*
@@ -127,12 +173,17 @@ class Prodalet
 	 
 		$o = get_option( $option_name );
 	 
-		switch ( $type ) {  
-			case 'text':  
-				$o[$id] = esc_attr( stripslashes($o[$id]) );
-				echo "<input class='regular-text' type='text' id='$id' name='" . $option_name . "[$id]' value='$o[$id]' />";  
-				echo (isset($args['desc'])) ? '<br /><span class="description">'.$args['desc'].'</span>' : "";  
-			break;
+		switch ( $type ) {
+            case 'text':
+                $o[$id] = esc_attr( stripslashes($o[$id]) );
+                if(isset($o[$id]) && $o[$id] !=='') {
+                    $val = $o[$id];
+                } elseif(isset($args['default'])) {
+                    $val = $args['default'];
+                }
+                echo "<input class='regular-text' type='text' id='$id' name='" . $option_name . "[$id]' value='$val' />";
+                echo (isset($args['desc'])) ? '<br /><span class="description">'.$args['desc'].'</span>' : "";
+            break;
 			case 'textarea':  
 				$o[$id] = esc_attr( stripslashes($o[$id]) );
 				echo "<textarea class='code large-text' cols='30' rows='10' type='text' id='$id' name='" . $option_name . "[$id]'>$o[$id]</textarea>";  
@@ -196,4 +247,3 @@ class Prodalet
 		return $options;
 	}
 }
-?>
--- prodalet.php
+++ prodalet.php
@@ -1,9 +1,9 @@
 <?php
 /*
 Plugin Name: ProdaLet
-Plugin URI: https://prodalet.ru/cms-wordpress/plugin-wt-yandex-metrika-for-cms-wordpress
+Plugin URI: https://prodalet.ru/cms-ext_plugins/wordpress/prodalet.zip
 Description: Подключение сервиса повышения конверсии ProdaLet.ru
-Version: 1
+Version: 1.2
 Author: ProdaLet
 Author URI: https://prodalet.ru
 */
@@ -18,6 +18,8 @@ class Prodalet
 		'script' => '',
 		'position' => 'header',
 		'mode' => 'all',
+		'lazy_load_desctop' => 0,
+		'lazy_load_mobile' => 0,
         'activate_admin_panel' => null
     );
 
@@ -56,18 +58,93 @@ class Prodalet
 
     // Подготавливаем код для вывода в шапке
     function wp_head() {
-		if (!empty( $this->options['script'])) echo $this->options['script'];
+        $this->renderCode();
 	}
 
     // Подготавливаем код для вывода в подвале
     public function action_wp_footer() {
-		if (!empty( $this->options['script'])) echo $this->options['script'];
+        $this->renderCode();
 	}
 
 	// Подготавливаем код для вывода в панели администратора
     public function action_admin_footer(){
-        if (!empty( $this->options['script'])) echo $this->options['script'];
+        $this->renderCode();
     }
+
+    public function renderCode()
+    {
+        if(preg_match('~\.int$~', $_SERVER['HTTP_HOST']))
+        {
+            $dbg = true;
+            $host = 'http://prodalet.int';
+        } else {
+            $host = 'https://prodalet.ru';
+        }
+
+        $api_key = $this->options['script_api_key'];
+        $lazy_load_time = 0;
+
+        if ($user = wp_get_current_user()) {
+            $js_client_info = array(
+                'first_name' => '',
+                'last_name' => '',
+                'phone' => '',
+                'email' => $user->data->user_email,
+                'client_id' => $user->data->ID,
+                'guest_id' => ""
+            );
+
+
+        } else {
+            //Если пользователь неизвестен
+            $js_client_info = array(
+                'first_name' => "",
+                'last_name' => "",
+                'phone' => "",
+                'email' => "",
+                'client_id' => "",
+                'guest_id' => ""
+            );
+        }
+
+        $js_client_info = json_encode($js_client_info);
+        $js_client_info_query = "PLTQ.push(['api', 'setClientInfo', " . $js_client_info . "]);";
+
+        //Формируем код для встраивания в страницу
+        $ret = "
+<!--Start Prodalet code WordPress plugin vers 1.2 {literal}-->
+<script>
+    lazy=0;lM=". $this->options['lazy_load_mobile']*1000 ."; lD=". $this->options['lazy_load_desctop']*1000 ."; if( window.matchMedia(\"only screen and (max-width: 760px)\").matches ) {lazy = lM;} else {lazy = lD;}
+    setTimeout(function (){ 
+        var PLTQ = []; {$js_client_info_query} var d = new Date().getTime();
+        
+        (function (d,w) { 
+            var n = d.getElementsByTagName(\"script\")[0], 
+            s = d.createElement(\"script\"), 
+            f = function () { n.parentNode.insertBefore(s, n); }; 
+            var dt = new Date().getTime(); 
+            s.type = \"text/javascript\"; 
+            s.id=\"plt_main\"; 
+            s.src=\"{$host}/bundles/api/main.js?h=\"+dt; 
+            s.setAttribute(\"data-plugin\", \"wordpress\"); 
+            s.setAttribute(\"data-pltkey\", \"{$api_key}\"); 
+            s.setAttribute(\"data-plthost\",\"{$host}\"); 
+            s.async = true; 
+            if (w.opera == \"[object Opera]\") { 
+                d.addEventListener(\"DOMContentLoaded\", f, false); 
+            } else { 
+                f(); 
+            } })(document,window);{$js_ready}
+        }
+        
+    ,lazy);</script>
+<!--End Prodalet code {/literal}-->";
+
+        print $ret;
+
+    }
+
+
 
 }
 
